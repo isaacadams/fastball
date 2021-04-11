36ca4fcbe264a20c1b5abc7465abec67d0154e5e
Isaac Adams <isaac.d.adams@gmail.com>
2021-04-11 15:12:29 -0400

    init source

---
+++ src/server/main.ts
@@ -0,0 +1,21 @@
+import { ids } from '../shared/coin.ids';
+import * as http from 'https';
+
+function get(url) {
+  http
+    .get(url, (r) => {
+      console.log(r.headers);
+      console.log(`${r.statusCode}: ${r.statusMessage}`);
+      r.on('data', function (d) {
+        process.stdout.write(d);
+      });
+    })
+    .on('error', console.error);
+}
+
+const domain = 'https://api.coingecko.com/api/v3/';
+const checkPrice = `${domain}simple/price?ids=${ids.BITCOIN}&vs_currencies=usd`;
+
+const getCoins = `${domain}coins/list`;
+
+get(checkPrice);
---
+++ src/shared/coin.ids.ts
@@ -0,0 +1,3 @@
+export const ids = {
+  BITCOIN: 'bitcoin',
+};
